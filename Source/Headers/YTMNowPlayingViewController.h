f4051e034b4129e0dc02e4424088684bb477edb5
Chiuhuang <ct@chiuhuang.dev>
2026-09-13 20:11:33 +0800

    lyrics: declare beside-3-dot methods to fix -Werror build

--- Source/Headers/YTMNowPlayingViewController.h
+++ Source/Headers/YTMNowPlayingViewController.h
@@ -11,6 +11,8 @@
 - (void)longPressNext:(UILongPressGestureRecognizer *)gesture;
 - (void)ytmu_keepLyricsButtonActive;
 - (void)ytmu_makeLyricsViewClickable:(UIView *)v;
+- (void)ytmuPlaceLyricsBesideThreeDot;
+- (UIView *)ytmu_findThreeDotControl:(UIView *)v;
 - (void)ytmu_didTapLyricsButtonAction:(id)sender;
 - (void)ytmu_didTapLyricsBar:(UITapGestureRecognizer *)gesture;
 - (BOOL)ytmu_replaceLyricsChip:(UIView *)official;
